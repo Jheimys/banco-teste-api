70bec7b7e464794cc4f4d4c71e3aaf8495c5f845
Jheimys Bassani <jheimysbb@gmail.com>
2026-03-20 16:05:12 +0000

    criando testes para o GET transferencias/{id}

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,11 +5,12 @@ const postTransferencia = require('../fixtures/postTransferencia.json');
 require('dotenv').config();
 
 describe('Transferencias', () => {
+    let token
+    beforeEach(async () => {
+        token = await obterToken('julio.lima', '123456')
+    })
+
     describe('POST /transferencias', () => {
-        let token
-        beforeEach(async () => {
-            token = await obterToken('julio.lima', '123456')
-        })
 
         it('Deve retornar sucesso 201 quando o valor da transferência for maior ou igual a 10.00', async() => {
             const bodyTransferencia = {...postTransferencia}
@@ -34,6 +35,24 @@ describe('Transferencias', () => {
 
             expect(resposta.status).to.equal(422)
             console.log(resposta.body)
+
+        })
+    })
+
+    describe('GET /transferencias/{id}', () => {
+        it('Deve retornar sucesso 200 quando a transferência existir com um id válido', async() => {
+            const resposta = await request(process.env.BASE_URL)
+            .get('/transferencias/1')
+            .set('Authorization', `Bearer ${token}`)
+
+            console.log(resposta.status)
+            console.log(resposta.body)
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.id).to.equal(1)
+            expect(resposta.body.id).to.be.a('number')
+            expect(resposta.body.valor).to.equal(200.50)
+            expect(resposta.body.conta_origem).to.equal(1)
+            expect(resposta.body.conta_destino).to.equal(2)
 
         })
     })
